44561ebf813f903073bbc53dbbb207f80fa56936
Steve <42344105+krypt007@users.noreply.github.com>
2021-12-10 14:37:28 +0300

    Update api_v2.spec.js

--- v2/tests/modules/api_v2.spec.js
+++ v2/tests/modules/api_v2.spec.js
@@ -1,11 +1,7 @@
-
-let mongoose = require("mongoose");
-let Api_v2 =  require('../../api_v2');
-
 let assert = require('chai').assert;
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../server');
+let server = 'https://SERVER_URL.cloudfunctions.net/api_v2';
 let expect = require('chai').expect;
 let should = require('chai').should();
 var chaiAsPromised = require('chai-as-promised');
@@ -13,34 +9,78 @@ chai.use(chaiAsPromised).should();
 chai.use(chaiHttp);
 
 
-describe('Api_v2', () => {
-    beforeEach((done) => {
-        Api_v2.remove({}, (err) => { 
-           done();           
-        });        
-    });
-  /*
-  * Test the /POST route
-  */
-  describe('/api/login username', () => {
-      it('it should not POST a book without pages field', (done) => {
-          let book = {
-              title: "The Lord of the Rings",
-              author: "J.R.R. Tolkien",
-              year: 1954
-          }
-        chai.request(server)
-            .post('/username')
-            .send(book)
+describe('API_V2', () => {
+  describe("POST /auth/signup", () => {
+    it("It should return an ok if signup is successful", (done) => {
+        const credentials = {
+            phoneNumber: "+254721234567",
+            countryCode: "KE",
+            password: "fsydhjashdfjksuYSzzDA3FUa88u4sP"
+        };
+        chai.request(server)                
+            .post("/auth/signup")
+            .send(credentials)
             .end((err, res) => {
-                  res.should.have.status(200);
-                  res.body.should.be.a('object');
-                  res.body.should.have.property('errors');
-                  res.body.errors.should.have.property('pages');
-                  res.body.errors.pages.should.have.property('kind').eql('required');
-              done();
-            });
-      });
+                res.should.have.status(201);
+                res.body.should.be.a('object');
+                res.body.should.have.property('status').eq('OK');
+            done();
+        });
+    });
+  });
 
+  describe("POST /api/login", () => {
+    it("It should return an accessToken", (done) => {
+        const credentials = {
+            phoneNumber: "+254721234567",
+            countryCode: "KE",
+            password: "fsydhjashdfjksuYSzzDA3FUa88u4sP"
+        };
+        chai.request(server)                
+            .post("/api/login")
+            .send(credentials)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('accessToken');
+            done();
+        });
+    });
+  });
+
+  //let's set up the data we need to pass to the login method
+  const userCredentials = {
+        phoneNumber: "+254721234567",
+        countryCode: "KE",
+        password: "fsydhjashdfjksuYSzzDA3FUa88u4sP"
+    };
+    //now let's login the user before we run any tests
+    var authenticatedUser = request.agent(server);
+    before(function(done){
+        authenticatedUser
+        .post('/api/login')
+        .send(userCredentials)
+        .end(function(err, response){
+            expect(response.statusCode).to.equal(200);
+            expect('accessToken', '/');
+            done();
+        });
+    });
+
+  describe("POST /kyc/user/create", () => {
+    it("It should return an ok if signup is successful", (done) => {
+        const userDetails = {
+            phoneNumber: "+254720123456"
+        };
+        chai.request(server)                
+            .post("/kyc/user/create")
+            .send(userDetails)
+            .end((err, res) => {
+                res.should.have.status(201); 
+                res.body.should.be.a('object');
+                res.body.should.have.property('userId').eq('668213f2fb5a177d6ef2aabcb77f1631e4eb9780');
+            done();
+        });
+    });
   });
 });
